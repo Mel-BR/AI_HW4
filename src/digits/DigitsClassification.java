1d78fd345e24f931937847edec2a52ab72f7c091
Melissa BR <melissa.benali-richard@etu.utc.fr>
2015-11-17 22:45:52 -0600

    Update main method to test the class Perceptron and Evaluator

--- src/digits/DigitsClassification.java
+++ src/digits/DigitsClassification.java
@@ -1,8 +1,11 @@
 package digits;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
+import digits.entities.Evaluator;
 import digits.entities.Parser;
+import digits.entities.Perceptron;
 import digits.entities.TestObservation;
 import digits.entities.TrainObservation;
 
@@ -18,18 +21,48 @@ public class DigitsClassification {
 		ArrayList<TestObservation> testObsList = Parser.buildTestObs("testimages","testlabels");
 
 		System.out.println("Done");
-
-		trainObsList.get(0).displayFeatures();
 		
-		System.out.println(trainObsList.get(0).getRealLabel());
+		Perceptron perceptron = new Perceptron(10, 28);
+		perceptron.train(trainObsList,1);
+		ArrayList<TestObservation> testObsListLabeled = perceptron.test(testObsList);
 		
-		trainObsList.get(1).displayFeatures();
+		/*
+		for (int i = 0; i < 8; i++) {
+			testObsListLabeled.get(i).displayFeatures();
+			System.out.println(testObsListLabeled.get(i).getPredictedLabel()+" "+testObsListLabeled.get(i).getRealLabel());
+		}
+		*/
 		
-		System.out.println(trainObsList.get(1).getRealLabel());	
+		Evaluator eval = new Evaluator(testObsListLabeled);
+		DecimalFormat percentFormatter = new DecimalFormat("00.00%");
 		
-		trainObsList.get(202).displayFeatures();
+		System.out.println();
 		
-		System.out.println(trainObsList.get(202).getRealLabel());
+		// Displaying overall accuracy
+		System.out.println("General accuracy : "+percentFormatter.format(eval.getGeneralAccuracy()));
+		
+		System.out.println();
+		
+		// Displaying accuracy for every number
+		for(int i=0;i<10;i++){
+			System.out.println("Accuracy for label "+i+" : "+percentFormatter.format(eval.getAccuracy(i)));
+		}
+		
+		System.out.println();
+		
+		// Displaying Confusion Matrix
+		System.out.println("Confusion matrix:");
+		float[][] confMat = eval.generateConfMatrix(testObsListLabeled);
+		DecimalFormat formatter = new DecimalFormat("00.00");
+		for(int i=0;i<10;i++){		
+			for(int j=0;j<10;j++){
+				System.out.printf(formatter.format(confMat[i][j]*100));
+				System.out.print("  ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
 		
 		
 	}
